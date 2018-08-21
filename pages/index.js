bbe33bf946db529bbb45fe88a33cadc361d67989
mrBettazzi <paolo.bettazzi@gmail.com>
2018-08-21 21:12:45 +0200

    data fetched

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Index = (props) => (
 )
 
 Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=casino')
   const data = await res.json()
 
   console.log(`Show data fetched. Count: ${data.length}`)
